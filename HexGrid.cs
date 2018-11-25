69d727635d1c78b260912b40095b98ddc0c9b432
Carl Baumann <friedyeti@gmail.com>
2018-11-24 23:53:07 -0600

    Tested BucketPriority and fixed mistakes

--- HexGrid.cs
+++ HexGrid.cs
@@ -5,29 +5,41 @@ using UnityEngine;
 
 public class BucketPriority<T> {
     private List<List<T>> queue;
-    private bool isMonotonic;
+    private readonly bool isMonotonic;
     private int monotonicIndex;
+    private int queueCount = 0;
 
-    public BucketPriority(bool isMonotonic = false) {
+    public BucketPriority(int approxMaxSize, bool isMonotonic = false) {
 	    queue = new List<List<T>>();
 	    this.isMonotonic = isMonotonic;
 	    monotonicIndex = 0;
+        for(int i = 0; i < approxMaxSize; i++) {
+            List<T> newList = new List<T>();
+            queue.Add(newList);
+        }
     }
 
     public void Enqueue(int priority, T newData) {
+        while(queue.Count <= priority) {
+            List<T> newList = new List<T>();
+            queue.Add(newList);
+        }
 	    if(isMonotonic && priority < monotonicIndex) {
-		    throw new Exception("Cannot add a lower priority than has been removed from a monotonic priority");
+		    throw new ArgumentException("Cannot add a lower priority than has been removed from a monotonic priority");
 	    }
+        queueCount++;
 	    queue[priority].Add(newData);
     }
 
     public T Dequeue() {
+        if(queueCount < 1) { return default(T); }
 	    if(isMonotonic) {
 		    while(queue[monotonicIndex].Count < 1) {
 			    monotonicIndex++;
 		    }
 		    T temp = queue[monotonicIndex][queue[monotonicIndex].Count -1];
 		    queue[monotonicIndex].RemoveAt(queue[monotonicIndex].Count -1);
+            queueCount--;
 		    return temp;
 	    }
 	else {
@@ -37,11 +49,13 @@ public class BucketPriority<T> {
 		}
 		T temp = queue[priorityIndex][queue[priorityIndex].Count -1];
 		queue[priorityIndex].RemoveAt(queue[priorityIndex].Count -1);
+        queueCount--;
 		return temp;
 	    }
     }
 
     public T Peak() {
+        if (queueCount < 1) { return default(T); }
         if (isMonotonic) {
             while (queue[monotonicIndex].Count < 1) {
                 monotonicIndex++;
@@ -57,7 +71,12 @@ public class BucketPriority<T> {
             T temp = queue[priorityIndex][queue[priorityIndex].Count - 1];
             return temp;
         }
+		
     }
+	
+	public bool Empty() {
+		return queueCount == 0;
+	}
 }
 
 public class HexGrid {
